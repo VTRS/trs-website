bbc8cf22aa8f087ee14679f1b573400678c69d02
VHTRS <A01701017@itesm.mx>
2020-11-17 19:13:54 -0600

    Light Box finished

--- trsfolio/static/js/glitch.js
+++
@@ -1,9 +0,0 @@
-function yourFunction() {
-    const hide = document.getElementById("hide");
-    hide.className =
-      hide.className === "hide"
-        ? (hide.className = "example removeHide")
-        : (hide.className = "hide");
-    setTimeout(yourFunction, 1500);
-}
-yourFunction();
