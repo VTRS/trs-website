e222f2eb4c8c7351622c808af4a770fdc3427446
VHTRS <A01701017@itesm.mx>
2020-11-21 18:53:12 -0600

    found error of static

--- trsfolio/trsfolio/settings/base.py
+++ trsfolio/trsfolio/settings/base.py
@@ -96,15 +96,5 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 LOGIN_REDIRECT_URL = '/pasajeros/consultar_pasajeros'
 LOGIN_URL = '/accounts/login/'
