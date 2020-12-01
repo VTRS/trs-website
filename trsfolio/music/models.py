adc6ef618513f2ada12b36af95b640c81821ce47
VHTRS <A01701017@itesm.mx>
2020-12-01 16:18:15 -0600

    change upload_to in all models

--- trsfolio/music/models.py
+++ trsfolio/music/models.py
@@ -1,14 +1,5 @@
 from django.db import models
 
-# Create your models here.
-def get_upload_track_path(instance, filename):
-    if (instance.ep):
-        model = instance.ep.name
-        name = model.replace(' ','_')
-        return f'tracks/{name}/{filename}'
-    else:
-        return f'tracks/singles/{filename}'
-
 class Ep(models.Model):
     name = models.CharField(max_length=255)
     artist = models.CharField(max_length=500)
@@ -18,7 +9,7 @@ class Ep(models.Model):
 
 class Song(models.Model):
     name = models.CharField(max_length=255)
-    file = models.FileField(upload_to=get_upload_track_path)
+    file = models.FileField(upload_to='audio')
     artist = models.CharField(max_length=500)
     ep = models.ForeignKey(
         Ep, 
--- trsfolio/shared/models.py
+++ trsfolio/shared/models.py
@@ -1,14 +1,5 @@
 from django.db import models
 
-# Create your models here.
-def get_upload_path(instance, filename):
-    model = instance.album.name
-    name = model.replace(' ', '_')
-    return f'/images/{name}'
-
-def get_upload_track_path(instance, filename):
-    return f'files'
-
 class Album(models.Model):
     name = models.CharField(max_length=50)
     
@@ -18,8 +9,8 @@ class Album(models.Model):
     
 class Image(models.Model):
     name = models.CharField(max_length=255)
-    image = models.ImageField(upload_to=get_upload_path)
-    thumbnail = models.ImageField(upload_to=get_upload_path, null=True, blank=True)
+    image = models.ImageField(upload_to='images')
+    thumbnail = models.ImageField(upload_to='images', null=True, blank=True)
     default = models.BooleanField(default=False)
     album = models.ForeignKey(Album, related_name='images', on_delete=models.CASCADE)
 
@@ -28,7 +19,7 @@ class Image(models.Model):
 
 class File(models.Model):
     name = models.CharField(max_length=255)
-    file = models.FileField(upload_to=get_upload_track_path)
+    file = models.FileField(upload_to='files')
 
     def __str__(self):
         return self.name
