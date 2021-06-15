7652e0a69acfebce3bf6b5696ec696332b5a40cf
VHTRS <A01701017@itesm.mx>
2021-06-15 11:59:20 -0500

    [TRS] navbar

--- trsfolio/photography/views.py
+++ trsfolio/photography/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import *
 
 # Create your views here.
 def gallery(request):
-    series = Series.objects.all()
-    return render(request, 'photography/gallery.html', {'series': series})
+    collection = Series.objects.all()
+    return render(request, 'photography/gallery.html', {'collection': collection})
+
+def series(request, id):
+    series = get_objecto_or_404(Series, id=id)
+    return render(request, 'photography/series.html', {'series':series})
