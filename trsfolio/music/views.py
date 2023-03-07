395cb94cfc4eb69fc7b4715615d8c5ab19a679eb
VHTRS <A01701017@itesm.mx>
2023-03-07 15:56:41 -0600

    [TRS] change order for events

--- trsfolio/music/views.py
+++ trsfolio/music/views.py
@@ -10,6 +10,6 @@ def discography(request):
     return render(request, 'music/discography.html', {'tracks': tracks})
 
 def events(request):
-    events = Event.objects.filter(end_date__gte=timezone.now()).order_by('-date')
-    past_events = Event.objects.filter(end_date__lt=timezone.now()).order_by('date')
+    events = Event.objects.filter(end_date__gte=timezone.now()).order_by('date')
+    past_events = Event.objects.filter(end_date__lt=timezone.now()).order_by('-date')
     return render(request, 'music/events.html', {'events': events, 'past_events': past_events})
