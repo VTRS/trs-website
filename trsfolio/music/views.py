b8fc0039e9843a2d996c23221c48f7b5af0c8030
VHTRS <A01701017@itesm.mx>
2023-03-07 15:33:46 -0600

    [TRS] reorder and different desing for past events

--- trsfolio/music/views.py
+++ trsfolio/music/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import *
+from django.utils import timezone
 
 
 # Create your views here.
@@ -9,5 +10,6 @@ def discography(request):
     return render(request, 'music/discography.html', {'tracks': tracks})
 
 def events(request):
-    events = Event.objects.all()
-    return render(request, 'music/events.html', {'events': events})
+    events = Event.objects.filter(end_date__gte=timezone.now()).order_by('-date')
+    past_events = Event.objects.filter(end_date__lt=timezone.now()).order_by('date')
+    return render(request, 'music/events.html', {'events': events, 'past_events': past_events})
