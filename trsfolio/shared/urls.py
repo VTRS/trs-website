d5b08478bc314e94f68a70b5eb74748e2e1f8b7b
TRS <victor@dxtr.mx>
2024-03-20 17:50:41 -0600

    [TRS] new error messages

--- trsfolio/shared/urls.py
+++ trsfolio/shared/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('links', views.links, name='links'),
     path('terms', views.terms, name='terms'),
-    path('contact', views.contact, name='contact')
+    path('contact', views.contact, name='contact'),
+    path('under_construction', views.under_construction, name='under_construction'),
+
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
